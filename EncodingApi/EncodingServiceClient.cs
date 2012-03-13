ae82324ec4f42d90443910c01829665811f9f7b7
sam <sam.bcen@gmail.com>
2012-03-12 22:54:56 -0400

    Added proper exception handling and addition comments.

--- EncodingApi/EncodingServiceClient.cs
+++ EncodingApi/EncodingServiceClient.cs
@@ -96,37 +96,78 @@ namespace EncodingApi
             return result.MediaList;
         }
         
-        protected string SendRequest(EncodingQuery qry)
+        /// <summary>
+        /// Sends a HttpWebRequest with EncodingQuery to Encoding.com and returns a 
+        /// xml result.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns>A xml string.</returns>
+        protected string SendRequest(EncodingQuery query)
         {
             if (UserId == null || UserKey == null)
                 throw new EncodingServiceException("UserId or UserKey is empty");
 
+            // Default xmlResult if nothing is read from the response.
             string xmlResult = "<response><error>Cannot establish a request to the server</error></response>";
+            
             HttpWebRequest request = CreateWebRequest();
-
             if (request != null)
             {
-                qry.UserId = UserId;
-                qry.UserKey = UserKey;
-                string content = "xml=" + qry.ToString();
-                qry = null;
+                query.UserId = UserId;
+                query.UserKey = UserKey;
+                string content = "xml=" + query.ToString();
+                query = null;
                 request.ContentLength = content.Length;
 
-                using (Stream stream = request.GetRequestStream())
+                // Writes EncodingQuery to the request stream.
+                Stream stream = null;
+                try
                 {
+                    stream = request.GetRequestStream();
                     stream.Write(Encoding.UTF8.GetBytes(content), 0, content.Length);
                 }
-
-                using (StreamReader reader = new StreamReader(request.GetResponse()
-                                                                     .GetResponseStream()))
+                catch (WebException ex)
                 {
-                    xmlResult = reader.ReadToEnd();
+                    throw new EncodingServiceException(ex.Message);
+                }
+                finally
+                {
+                    if (stream != null)
+                    {
+                        stream.Close();
+                    }
+                }
+
+                // Reads xml string from response using StreamReader.
+                WebResponse response = null;
+                try
+                {
+                    response = request.GetResponse();
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        xmlResult = reader.ReadToEnd();
+                    }
+                }
+                catch (WebException ex)
+                {
+                    throw new EncodingServiceException(ex.Message);
+                }
+                finally
+                {
+                    if (response != null)
+                    {
+                        response.Close();
+                    }
                 }
             }
 
             return xmlResult;
         }
 
+        /// <summary>
+        /// Creates a HttpWebRequest.
+        /// </summary>
+        /// <returns>A HttpWebRequest object.</returns>
         protected HttpWebRequest CreateWebRequest()
         {
             Uri host = UseSslConnection ? DefaultSslHost : DefaultHost;
