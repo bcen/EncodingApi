ca67cf9b2f09865229378f1f0c87b898b65bb06d
sam <sam.bcen@gmail.com>
2012-03-16 16:17:26 -0400

    Fully documentated BasicResponse and GetMediaListResponse class.

--- EncodingApi/BasicResponse.cs
+++ EncodingApi/BasicResponse.cs
@@ -1,43 +1,47 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Xml.Serialization;
-using System;
 
 namespace EncodingApi
 {
+    /// <summary>
+    /// Base class for all response class.
+    /// </summary>
     [XmlRoot("response")]
     public abstract class BasicResponse
     {
+        /// <summary>
+        /// Message from response.
+        /// </summary>
         [XmlElement("message")]
         public string Message { get; set; }
 
+        /// <summary>
+        /// List of error.
+        /// </summary>
         [XmlArray("errors")]
         [XmlArrayItem("error")]
-        public List<string> Errors
-        {
-            get
-            {
-                return (_errors ?? (_errors = new List<string>()));
-            }
-            set
-            {
-                _errors = value;
-            }
-        }
-        private List<string> _errors;
+        public List<string> Errors { get; set; }
 
+        /// <summary>
+        /// To test whether to serialize Message or not.
+        /// </summary>
+        /// <returns>True if Message is not null nor empty string, otherwise false.</returns>
+        public bool ShouldSerializeMessage() { return !String.IsNullOrEmpty(Message); }
+
+        /// <summary>
+        /// To test wether to serialize Errors or not.
+        /// </summary>
+        /// <returns>True if Errors count is greater than zero, otherwise false.</returns>
+        public bool ShouldSerializeErrors() { return (Errors.Count > 0); }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public BasicResponse()
         {
             Message = string.Empty;
-        }
-
-        public bool ShouldSerializeMessage()
-        {
-            return !String.IsNullOrEmpty(Message);
-        }
-
-        public bool ShouldSerializeErrors()
-        {
-            return (_errors != null);
+            Errors = new List<string>();
         }
     }
 }
--- EncodingApi/GetMediaListResponse.cs
+++ EncodingApi/GetMediaListResponse.cs
@@ -6,53 +6,98 @@ using System.Xml.Serialization;
 
 namespace EncodingApi
 {
+    /// <summary>
+    /// Encapsulates GetMediaList XML response.
+    /// </summary>
     [XmlRoot("response")]
     public class GetMediaListResponse : BasicResponse
     {
+        /// <summary>
+        /// List of media meta from server.
+        /// </summary>
         [XmlElement("media")]
-        public List<Media> MediaList
-        {
-            get
-            {
-                return (_mediaList ?? (_mediaList = new List<Media>()));
-            }
-            set
-            {
-                _mediaList = value;
-            }
-        }
-        private List<Media> _mediaList;
+        public List<Media> MediaList { get; set; }
 
+        /// <summary>
+        /// To test wether to serialize MediaList or not.
+        /// </summary>
+        /// <returns>True if media list count is greater than zero, otherwise false.</returns>
+        public bool ShouldSerializeMediaList() { return (MediaList.Count > 0); }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public GetMediaListResponse()
         {
+            MediaList = new List<Media>();
         }
 
+        /// <summary>
+        /// Media meta class.
+        /// </summary>
         public class Media
         {
+            /// <summary>
+            /// The source URL of the media.
+            /// </summary>
             [XmlElement("mediafile")]
             public string MediaFile { get; set; }
 
+            /// <summary>
+            /// The ID of the media.
+            /// </summary>
             [XmlElement("mediaid")]
             public string MediaId { get; set; }
             
+            /// <summary>
+            /// The status of the media.
+            /// </summary>
             [XmlElement("mediastatus")]
             public string MediaStatus { get; set; }
 
+            /// <summary>
+            /// The date when the media is first created in the queue.
+            /// </summary>
             [XmlElement("createdate")]
             public string CreateDate { get; set; }
 
+            /// <summary>
+            /// The date when the media is first started in the processing queue.
+            /// </summary>
             [XmlElement("startdate")]
             public string StartDate { get; set; }
 
+            /// <summary>
+            /// The date when the media is finished.
+            /// </summary>
             [XmlElement("finishdate")]
             public string FinishDate { get; set; }
 
+            /// <summary>
+            /// To test whether serialize MediaId or not.
+            /// </summary>
+            /// <returns>True if MediaId is not null nor empty string, otherwise false.</returns>
+            public bool ShouldSerializeMediaId() { return !String.IsNullOrEmpty(MediaId); }
+
+            /// <summary>
+            /// To test whether serialize MediaStatus or not.
+            /// </summary>
+            /// <returns>True if MediaStatus is not null nor empty string, otherwise false.</returns>
+            public bool ShouldSerializeMediaStatus() { return !String.IsNullOrEmpty(MediaStatus); }
+
+            /// <summary>
+            /// Default constructor.
+            /// </summary>
             public Media()
             {
                 MediaId = String.Empty;
                 MediaStatus = String.Empty;
             }
 
+            /// <summary>
+            /// Converts MediaFile URL string to Uri.
+            /// </summary>
+            /// <returns>An instance of Uri class.</returns>
             public Uri GetMediaFileUri()
             {
                 Uri uri = null;
@@ -63,6 +108,10 @@ namespace EncodingApi
                 return uri;
             }
 
+            /// <summary>
+            /// Converts CreateDate string representation to DateTime.
+            /// </summary>
+            /// <returns>An instance of DateTime for CreateDate.</returns>
             public DateTime GetCreateDate()
             {
                 DateTime d = DateTime.MinValue;
@@ -73,6 +122,10 @@ namespace EncodingApi
                 return d;
             }
 
+            /// <summary>
+            /// Converts StartDate string representation to DateTime.
+            /// </summary>
+            /// <returns>An instance of DateTime for StartDate.</returns>
             public DateTime GetStartDate()
             {
                 DateTime d = DateTime.MinValue;
@@ -83,6 +136,10 @@ namespace EncodingApi
                 return d;
             }
 
+            /// <summary>
+            /// Converts FinishDate string representation to DateTime.
+            /// </summary>
+            /// <returns>An instance of DateTime for FinishDate.</returns>
             public DateTime GetFinishDate()
             {
                 DateTime d = DateTime.MinValue;
@@ -91,16 +148,6 @@ namespace EncodingApi
                     DateTime.TryParse(FinishDate, out d);
                 }
                 return d;
-            }
-
-            public bool ShouldSerializeMediaId()
-            {
-                return !String.IsNullOrEmpty(MediaId);
-            }
-
-            public bool ShouldSerializeMediaStatus()
-            {
-                return !String.IsNullOrEmpty(MediaStatus);
             }
         }
     }
