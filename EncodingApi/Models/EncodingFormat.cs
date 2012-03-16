c67c4c8e2d6289281640495e20044d26626953da
sam <sam.bcen@gmail.com>
2012-03-16 16:31:31 -0400

    Added documentation for EncodingFormat class.

--- EncodingApi/Models/EncodingFormat.cs
+++ EncodingApi/Models/EncodingFormat.cs
@@ -13,48 +13,72 @@ namespace EncodingApi.Models
         public int? NoiseReduction { get; set; }
 
         /// <summary>
-        /// Format type.
+        /// The output format type.
         /// </summary>
         [XmlElement("output")]
         public string Output { get; set; }
 
         /// <summary>
         /// Video frame size. Do not call the property directly, instead use
-        /// GetVideoFrameSize and SetVideoFrameSize to change this property.
+        /// GetVideoFrameSize and SetVideoFrameSize to mutate this property.
         /// </summary>
         [XmlElement("size")]
         public string Size { get; set; }
 
         /// <summary>
+        /// To test whether to serialize NoiseReduction or not.
+        /// </summary>
+        /// <returns>True if NoiseReduction is assigned with a value, otherwise false.</returns>
+        public bool ShouldSerializeNoiseReduction() { return NoiseReduction.HasValue; }
+
+        /// <summary>
+        /// To test whether to serialize Output or not.
+        /// </summary>
+        /// <returns>True if Output is not null nor empty string, otherwise false.</returns>
+        public bool ShouldSerializeOutput() { return !String.IsNullOrEmpty(Output); }
+
+        /// <summary>
+        /// To test whether to serialize Size or not.
+        /// </summary>
+        /// <returns>True if Size is not null nor empty string, otherwise false.</returns>
+        public bool ShouldSerializeSize() { return !String.IsNullOrEmpty(Size); }
+
+        /// <summary>
         /// Default constructor.
         /// </summary>
         public EncodingFormat()
+            : this(String.Empty)
         {
         }
 
         /// <summary>
-        /// Create a new instance with the specified output type.
+        /// Initializes a new instance with the specified output type.
         /// </summary>
         /// <param name="output">The output format type.</param>
         public EncodingFormat(string output)
         {
             Output = output;
+            Size = String.Empty;
         }
 
         /// <summary>
-        /// To test should NoiseReduction be serialized.
+        /// Sets the video frame size with the specified width and height.
         /// </summary>
-        /// <returns>Ture if NoiseReduction has value, else false.</returns>
-        public bool ShouldSerializeNoiseReduction()
-        {
-            return NoiseReduction.HasValue;
-        }
-
+        /// <param name="width">The width of the new video frame size.</param>
+        /// <param name="height">The hheight of the new video frame size.</param>
         public void SetVideoFrameSize(int width, int height)
         {
             SetVideoFrameSize(new VideoFrameSize(width, height));
         }
 
+        /// <summary>
+        /// Sets the video frame size with another instance of VideoFrameSize.
+        /// The new width and height must be an even integer.
+        /// </summary>
+        /// <param name="videoFrameSize">The new VideoFrameSize.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// If the new width or height is not an even integer.
+        /// </exception>
         public void SetVideoFrameSize(VideoFrameSize videoFrameSize)
         {
             if ((videoFrameSize.Width % 2 != 0) || (videoFrameSize.Height % 2 != 0))
@@ -63,10 +87,14 @@ namespace EncodingApi.Models
             Size = String.Format("{0}x{1}", videoFrameSize.Width, videoFrameSize.Height);
         }
 
+        /// <summary>
+        /// Gets the current VideoFrameSize.
+        /// </summary>
+        /// <returns>The current VideoFrameSize.</returns>
         public VideoFrameSize GetVideoFrameSize()
         {
             VideoFrameSize vfSize = null;
-            if (Size != null)
+            if (!String.IsNullOrEmpty(Size))
             {
                 string[] tokens = Size.Split('x', 'X');
                 vfSize = new VideoFrameSize(Convert.ToInt32(tokens[0]), Convert.ToInt32(tokens[1]));
@@ -75,35 +103,37 @@ namespace EncodingApi.Models
         }
     }
 
+    /// <summary>
+    /// Representation of video frame size.
+    /// </summary>
     public class VideoFrameSize
     {
+        /// <summary>
+        /// The width of the video frame.
+        /// </summary>
         public int Width { get; set; }
+
+        /// <summary>
+        /// The height of the video frame.
+        /// </summary>
         public int Height { get; set; }
 
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public VideoFrameSize()
         {
         }
 
+        /// <summary>
+        /// Initializes with the specified width and height.
+        /// </summary>
+        /// <param name="width">The width of the video frame.</param>
+        /// <param name="height">The height of the video frame.</param>
         public VideoFrameSize(int width, int height)
         {
             Width = width;
             Height = height;
-        }
-    }
-
-    public class FadeTime
-    {
-        public double Start { get; set; }
-        public double Duration { get; set; }
-
-        public FadeTime()
-        {
-        }
-
-        public FadeTime(double start, double duration)
-        {
-            Start = start;
-            Duration = duration;
         }
     }
 }
