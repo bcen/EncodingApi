f6e9b569d47e8aa841a65ad1daa80af3a3804269
sam <sam.bcen@gmail.com>
2012-03-16 16:19:53 -0400

    Removed unused using statement.

--- EncodingApi/AddMediaResponse.cs
+++ EncodingApi/AddMediaResponse.cs
@@ -1,25 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Xml.Serialization;
 
 namespace EncodingApi
 {
+    /// <summary>
+    /// Encapsulates AddMedia XML response.
+    /// </summary>
     [XmlRoot("response")]
     public class AddMediaResponse : BasicResponse
     {
+        /// <summary>
+        /// The ID of the added media.
+        /// </summary>
         [XmlElement("MediaID")]
         public string MediaId { get; set; }
 
+        /// <summary>
+        /// To test whether to serialize MediaId or not.
+        /// </summary>
+        /// <returns>True if MediaId is not null nor empty string, otherwise false.</returns>
+        public bool ShouldSerializeMediaId() { return !String.IsNullOrEmpty(MediaId); }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public AddMediaResponse()
         {
             MediaId = String.Empty;
-        }
-
-        public bool ShouldSerializeMediaId()
-        {
-            return !String.IsNullOrEmpty(MediaId);
         }
     }
 }
