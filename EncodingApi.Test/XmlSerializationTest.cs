14686dac3c82a627b6a87bd5e03a549bcbcfdc49
sam <sam.bcen@gmail.com>
2012-03-23 00:40:49 -0400

    Added test case for deserialization.

--- EncodingApi.Test/XmlSerializationTest.cs
+++ EncodingApi.Test/XmlSerializationTest.cs
@@ -1,10 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
-using System.Collections.Generic;
 using Xunit;
 
 namespace EncodingApi.Test
@@ -19,7 +18,6 @@ namespace EncodingApi.Test
                 UserId = "id",
                 UserKey = "key",
                 Action = "GetMediaList",
-                MediaId = "1234",
                 Notify = new Uri("mailto://someone@gmail.com"),
                 IsInstant = true,
                 Sources = new List<Uri>()
@@ -31,9 +29,17 @@ namespace EncodingApi.Test
                 {
                     new EncodingFormat()
                     {
-                        Output = "flv",
                         NoiseReduction = 3,
-                        AudioSampleRate = 0
+                        Output = "flv",
+                        VideoCodec = "vp6",
+                        AudioCodec = "libmp3lame",
+                        VideoBitrate = 0,
+                        AudioBitrate = 1.5,
+                        AudioSampleRate = 4000
+                    },
+                    new EncodingFormat()
+                    {
+                        Output = "mp3"
                     }
                 }
             };
@@ -44,7 +50,6 @@ namespace EncodingApi.Test
                 <userid>id</userid>
                 <userkey>key</userkey>
                 <action>GetMediaList</action>
-                <mediaid>1234</mediaid>
                 <source>http://www.google.com/example1.mp3</source>
                 <source>http://www.facebook.com/example2.flv</source>
                 <notify>mailto://someone@gmail.com</notify>
@@ -52,6 +57,13 @@ namespace EncodingApi.Test
                 <format>
                     <noise_reduction>3</noise_reduction>
                     <output>flv</output>
+                    <video_codec>vp6</video_codec>
+                    <audio_codec>libmp3lame</audio_codec>
+                    <audio_bitrate>1.5k</audio_bitrate>
+                    <audio_sample_rate>4000</audio_sample_rate>
+                </format>
+                <format>
+                    <output>mp3</output>
                 </format>
             </query>";
             string actualXml = Serialize(q, "    ");
@@ -63,7 +75,53 @@ namespace EncodingApi.Test
         [Fact]
         public void TestDeserialization()
         {
+            string xml =
+            @"<?xml version=""1.0"" encoding=""utf-8""?>
+            <query>
+                <userid>id</userid>
+                <userkey>key</userkey>
+                <action>GetMediaList</action>
+                <source>http://www.google.com/example1.mp3</source>
+                <source>http://www.facebook.com/example2.flv</source>
+                <notify>mailto://someone@gmail.com</notify>
+                <instant>yes</instant>
+                <format>
+                    <noise_reduction>3</noise_reduction>
+                    <output>flv</output>
+                    <video_codec>vp6</video_codec>
+                    <audio_codec>libmp3lame</audio_codec>
+                    <audio_bitrate>1.5k</audio_bitrate>
+                    <audio_sample_rate>4000</audio_sample_rate>
+                </format>
+                <format>
+                    <output>mp3</output>
+                </format>
+            </query>";
 
+            EncodingQuery q = Deserialize<EncodingQuery>(xml);
+
+            Assert.NotNull(q);
+            Assert.Equal("id", q.UserId);
+            Assert.Equal("key", q.UserKey);
+            Assert.Equal("GetMediaList", q.Action);
+            Assert.Contains(new Uri("http://www.google.com/example1.mp3"), q.Sources);
+            Assert.Contains(new Uri("http://www.facebook.com/example2.flv"), q.Sources);
+            Assert.Equal(new Uri("mailto://someone@gmail.com"), q.Notify);
+            Assert.True(q.IsInstant);
+
+            EncodingFormat f1 = q.Formats.ElementAtOrDefault(0);
+            EncodingFormat f2 = q.Formats.ElementAtOrDefault(1);
+
+            Assert.NotNull(f1);
+            Assert.Equal(3, f1.NoiseReduction);
+            Assert.Equal("flv", f1.Output);
+            Assert.Equal("vp6", f1.VideoCodec);
+            Assert.Equal("libmp3lame", f1.AudioCodec);
+            Assert.Equal(1.5D, f1.AudioBitrate);
+            Assert.Equal(4000, f1.AudioSampleRate);
+
+            Assert.NotNull(f2);
+            Assert.Equal("mp3", f2.Output);
         }
 
         public virtual string Serialize<T>(T obj, string indentChars) where T : class, new()
